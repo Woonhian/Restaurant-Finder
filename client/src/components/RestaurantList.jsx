d61821e501b92f9d3b9dd35492c6e58121d0bd5b
Woonhian <limwoonhian@hotmail.com>
2020-06-26 17:35:58 +0800

    Update restaurant functionality

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useContext} from 'react';
 import RestaurantFinder from '../apis/RestaurantFinder';
 import { RestaurantsContext } from '../context/RestaurantsContext';
+import { useHistory } from 'react-router-dom';
 
 const RestaurantList = (props) => {
     const {restaurants, setRestaurants} = useContext(RestaurantsContext);
+    let history = useHistory();
     useEffect(() => {
         const fetchData = async() => {
             try {
@@ -18,7 +20,7 @@ const RestaurantList = (props) => {
     }, []);
 
     const handleDelete = async(id) => {
-        try{
+        try {
             const response = await RestaurantFinder.delete(`/${id}`);
             setRestaurants(restaurants.filter(restaurant => {
                 return restaurant.id !== id;
@@ -27,6 +29,10 @@ const RestaurantList = (props) => {
         } catch(err) {
             console.log(err);
         }
+    };
+
+    const handleUpdate = (id) => {
+        history.push(`/restaurants/${id}/update`);
     }
 
     return (
@@ -50,7 +56,7 @@ const RestaurantList = (props) => {
                                 <td>{restaurant.location}</td>
                                 <td>{"$".repeat(restaurant.price_range)}</td>
                                 <td>Reviews</td>
-                                <td><button className="btn btn-warning">Update</button></td>
+                                <td><button onClick={() => handleUpdate(restaurant.id)} className="btn btn-warning">Update</button></td>
                                 <td><button onClick={() => handleDelete(restaurant.id)} className="btn btn-danger">Delete</button></td>
                             </tr>
                         );
