f08c409c40cef8932c53581f2bf535911e814f2d
Woonhian <limwoonhian@hotmail.com>
2020-06-26 15:39:01 +0800

    Added layout for home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,17 @@ import UpdatePage from './routes/UpdatePage';
 import RestaurantDetailPage from './routes/RestaurantDetailPage';
 
 const App = () => {
-    return <div>
-        <Router>
-            <Switch>
-                <Route exact path = "/" component={Home} />
-                <Route exact path = "/restaurants/:id/update" component={UpdatePage} />
-                <Route exact path = "/restaurants/:id" component={RestaurantDetailPage} />
-            </Switch>
-        </Router>
-    </div>
+    return (
+        <div className="container">
+            <Router>
+                <Switch>
+                    <Route exact path = "/" component={Home} />
+                    <Route exact path = "/restaurants/:id/update" component={UpdatePage} />
+                    <Route exact path = "/restaurants/:id" component={RestaurantDetailPage} />
+                </Switch>
+            </Router>
+        </div>
+    )
 }
 
 export default App;
